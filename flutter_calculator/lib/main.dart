0ef530c75096736154198bdaf6ab52693751cfb6
EroMCakes <salah17afifi@gmail.com>
2021-06-26 01:38:41 +0100

    added calculation feater with buttons

--- flutter_calculator/lib/main.dart
+++ flutter_calculator/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_calculator/buttons.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 void main() {
   runApp(MyApp());
@@ -89,20 +90,72 @@ class _HomePageState extends State<HomePage> {
                         crossAxisCount: 4),
                     itemBuilder: (BuildContext context, int index) {
                       switch (index) {
+                        // C button
                         case 0:
                           return MyButton(
+                            buttonTapped: () {
+                          setState(() {
+                            userQuestion = '';
+                            userAnswer = '';
+                          });
+                        },
                             buttonText: buttons[index],
                             color: Colors.green,
                             textColor: Colors.white,
                           );
+
+                          //Delete button
                         case 1:
                           return MyButton(
+                            buttonTapped: () {
+                          setState(() {
+                            if (userQuestion != '')
+                             userQuestion = userQuestion.substring(0, userQuestion.length - 1);
+                          });
+                        },
+                            buttonText: buttons[index],
+                            color: Colors.red,
+                            textColor: Colors.white,
+                          );
+
+                          //Equal button
+                          case 19:
+                          return MyButton(
+                            buttonTapped: () {
+                          setState(() {
+                            equalPressed();
+                          });
+                        },
                             buttonText: buttons[index],
                             color: Colors.red,
                             textColor: Colors.white,
                           );
                       }
                       return MyButton(
+                        buttonTapped: () {
+                          setState(() {
+                            if (isOperator(buttons[index]))
+                            {
+                              if (!isOperator(userQuestion[userQuestion.length - 1]))
+                              {
+                                if (userAnswer != '')
+                                {
+                                  userQuestion = userAnswer + buttons[index];
+                                  userAnswer = '';
+                                }
+                                else
+                                  userQuestion += buttons[index];
+                              }
+                            }
+                            else if (userAnswer != '')
+                              {
+                                userAnswer = '';
+                                userQuestion = '' + buttons[index];
+                              }
+                            else
+                              userQuestion += buttons[index];
+                          });
+                        },
                         buttonText: buttons[index],
                         color: isOperator(buttons[index])
                             ? Colors.deepPurple
@@ -123,5 +176,18 @@ class _HomePageState extends State<HomePage> {
       return true;
     } else
       return false;
+  }
+
+  void equalPressed()
+  {
+    String finalQuestion = userQuestion;
+    finalQuestion = finalQuestion.replaceAll('x', '*');
+    
+    Parser p = Parser();
+    Expression exp = p.parse(finalQuestion);
+    ContextModel cm = ContextModel();
+    double eval = exp.evaluate(EvaluationType.REAL, cm);
+
+    userAnswer = eval.toString();
   }
 }
